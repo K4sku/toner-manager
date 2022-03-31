4ceabccc705980ef2ab7ecd462511b54d2dd2595
Cezary Kłos <cezary.klos@gmail.com>
2022-03-31 09:50:15 +0200

    change scopes

--- app/models/toner.rb
+++ app/models/toner.rb
@@ -1,22 +1,17 @@
 class Toner < ApplicationRecord
-    belongs_to :toner_model, foreign_key: "toner_model_id"
-    belongs_to :printer, foreign_key: "printer_id"
+  belongs_to :toner_model, foreign_key: 'toner_model_id'
+  belongs_to :printer, foreign_key: 'printer_id'
 
-    validate :toner_cannot_be_spent_if_is_used
+  validate :toner_cannot_be_spent_if_is_used
 
-    scope :black, -> { joins(:toner_model).where(toner_models: {color: "black"})}
-    scope :cyan, -> { joins(:toner_model).where(toner_models: {color: "cyan"})}
-    scope :magenta, -> { joins(:toner_model).where(toner_models: {color: "magenta"})}
-    scope :yellow, -> { joins(:toner_model).where(toner_models: {color: "yellow"})}
+  scope :in_use, -> { joins(:toner_model).where(is_used: true) }
 
-    scope :black_in_use, -> { black.where(is_used: true)}
-    scope :cyan_in_use, -> { cyan.where(is_used: true)}
-    scope :magenta_in_use, -> { magenta.where(is_used: true)}
-    scope :yellow_in_use, -> { yellow.where(is_used: true)}
+  scope :black_in_use, -> { in_use.where(toner_models: { color: 'black' }) }
+  scope :cyan_in_use, -> { in_use.where(toner_models: { color: 'cyan' }) }
+  scope :magenta_in_use, -> { in_use.where(toner_models: { color: 'magenta' }) }
+  scope :yellow_in_use, -> { in_use.where(toner_models: { color: 'yellow' }) }
 
-    def toner_cannot_be_spent_if_is_used
-        if is_used && is_spent
-            errors.add(:is_spent, "Cannot be spent if is_used, change is_used flag")
-        end
-    end
+  def toner_cannot_be_spent_if_is_used
+    errors.add(:is_spent, 'Cannot be spent if is_used, change is_used flag') if is_used && is_spent
+  end
 end
