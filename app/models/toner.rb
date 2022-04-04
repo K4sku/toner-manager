3a81439df25b3c4f2628ef650739c715a2885309
Cezary Kłos <cezary.klos@gmail.com>
2022-04-04 21:40:42 +0200

    set printer to bullable

--- app/models/toner.rb
+++ app/models/toner.rb
@@ -1,10 +1,12 @@
 class Toner < ApplicationRecord
   belongs_to :toner_model, foreign_key: 'toner_model_id'
-  belongs_to :printer, foreign_key: 'printer_id'
+  belongs_to :printer, foreign_key: 'printer_id', optional: true
 
   validate :toner_cannot_be_spent_if_is_used
 
   scope :in_use, -> { joins(:toner_model).where(is_used: true) }
+
+  scope :unused, -> { where(printer_id: nil) }
 
   scope :black_in_use, -> { in_use.where(toner_models: { color: 'black' }) }
   scope :cyan_in_use, -> { in_use.where(toner_models: { color: 'cyan' }) }
